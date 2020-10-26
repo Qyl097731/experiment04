93e3078f4a95cb50b659ec5cde1bdcf44d97977b
邱依良 <553579048@qq.com>
2020-10-26 20:44:25 +0800

    Initial commit

---
+++ src/main/java/com/njxzc/NjxzcApplication.java
@@ -0,0 +1,13 @@
+package com.njxzc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NjxzcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NjxzcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/njxzc/NjxzcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.njxzc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NjxzcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
